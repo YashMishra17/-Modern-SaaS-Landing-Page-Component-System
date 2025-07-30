f7bcdb43737e41004f3260458392471de38480f0
YashMishra17 <110155847+YashMishra17@users.noreply.github.com>
2025-07-30 22:58:51 +0530

    _page.tsx

---
+++ app/_page.tsx
@@ -0,0 +1,19 @@
+import Hero from './components/sections/Hero'
+import Features from './components/sections/Features'
+import Pricing from './components/sections/Pricing'
+import Testimonials from './components/sections/Testimonials'
+import FAQ from './components/sections/FAQ'
+import Footer from './components/sections/Footer'
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Features />
+      <Pricing />
+      <Testimonials />
+      <FAQ />
+      <Footer />
+    </main>
+  )
+}
